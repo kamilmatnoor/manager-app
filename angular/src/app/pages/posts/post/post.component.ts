7a4ce442d799874e5a21fdc96d77ede2ef022143
Muhamad Kamil <kamilmatnoor@gmail.com>
2020-04-21 10:57:32 +0800

    Updated code to display single post with list of comments

--- angular/src/app/pages/posts/post/post.component.ts
+++ angular/src/app/pages/posts/post/post.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsService } from 'src/app/services/posts/posts.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -10,16 +10,30 @@ import { ActivatedRoute } from '@angular/router';
 export class PostComponent implements OnInit {
 
   post: any = {}
+  comments: any = []
 
   constructor(
     private postsService: PostsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
-    let id = this.route.snapshot.paramMap.get("id")
-    this.postsService.getPostById(id).then(data => {
-      this.post = data
+    this.route.queryParams.subscribe(params => {
+      this.postsService.getAllCommentsByPostId(params.postId).then(data => {
+        this.comments = data
+      })
+
+      this.postsService.getPostById(params.postId).then(data => {
+        this.post = data
+        console.log(data)
+      })
+    })
+  }
+
+  onBackClicked() {
+    this.router.navigate([`posts`], {
+      queryParams: {}
     })
   }
 }
